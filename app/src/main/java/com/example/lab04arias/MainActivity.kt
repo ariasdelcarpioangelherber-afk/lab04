eb3493452b1614c8a11f659a3e03e03cab2f80f3
Angel Arias del Carpio <ariasdelcarpioangelherber@gmail.com>
2025-09-10 14:56:31 -0500

    LazyColumn

--- app/src/main/java/com/example/lab04arias/MainActivity.kt
+++ app/src/main/java/com/example/lab04arias/MainActivity.kt
@@ -3,27 +3,30 @@ package com.example.lab04arias
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab04arias.ui.theme.Lab04AriasTheme
+import androidx.compose.ui.unit.dp
+import com.example.lab04arias.ui.theme.Lab04AriasTheme // <- CORREGIDO AQUÍ
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Lab04AriasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Lab04AriasTheme { // <- CORREGIDO AQUÍ
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyLazyColumn()
                 }
             }
         }
@@ -31,17 +34,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyLazyColumn() {
+    val items = (1..50).map { "Item número $it" }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(items) { item ->
+            Text(
+                text = item,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     Lab04AriasTheme {
-        Greeting("Android")
+        MyLazyColumn()
     }
 }
