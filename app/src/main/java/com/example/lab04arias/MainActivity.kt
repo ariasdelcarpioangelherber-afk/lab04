3c5e403bb8a0e32088fd0184ad8816f479c46abf
ariasdelcarpioangelherber-afk <ariasdelcarpioangelherber@gmail.com>
2025-09-14 21:56:58 -0500

    Change from GitHub

--- app/src/main/java/com/example/lab04arias/MainActivity.kt
+++ app/src/main/java/com/example/lab04arias/MainActivity.kt
@@ -52,7 +52,7 @@ fun MyLazyVerticalGrid() {
                 modifier = Modifier.padding(8.dp)
             ) {
                 Text(
-                    text = item,
+                    Text(text = "Hola, soy el cambio desde GitHub") // <-- Cambio desde la web
                     modifier = Modifier.padding(16.dp)
                 )
             }
